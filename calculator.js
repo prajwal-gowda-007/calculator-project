a68c852b41549dd464eed995f511810c62de4d07
Prajwal Gowda <200454372+prajwal-gowda-007@users.noreply.github.com>
2026-05-17 20:43:19 +0530

    Integrate calculator UI and functionality

--- calculator.js
+++ calculator.js
@@ -30,9 +30,65 @@ function operate(a,b,operator)
             return add(a,b);
         case "-":
             return subtract(a,b);
-        case "*":
+        case "×":
             return multiply(a,b);
-        case "/":
+        case "÷":
             return divide(a,b);
     }
 };
+
+const display = document.querySelector(".display")
+const numbers = document.querySelectorAll(".numbers");
+const operators =document.querySelectorAll(".operators");
+const equals=document.querySelector(".equals");
+const allClear = document.querySelector(".allClear")
+const delBtn = document.querySelector(".delete");
+const clear = document.querySelector(".clear")
+
+display.textContent="";
+numbers.forEach(number => 
+    {
+        number.addEventListener("click",() => 
+        { 
+            if( display.textContent.includes(".") && number.textContent===".")
+                { return }
+            display.textContent+=number.textContent;
+        })
+    })
+
+operators.forEach(operation => 
+    { operation.addEventListener("click",() =>
+        {
+            firstNum=display.textContent;
+            display.textContent=""
+            display.textContent=operation.textContent;
+            operator=display.textContent;
+            display.textContent="";
+        })
+    })
+
+equals.addEventListener("click",() =>
+    {
+        secondNum=display.textContent;
+        let result = operate(+firstNum,+secondNum,operator);
+        display.textContent=result;
+    })
+
+allClear.addEventListener("click",()=>
+    {
+        firstNum ="";
+        secondNum ="";
+        operator ="";
+        display.textContent="";
+    })
+
+delBtn.addEventListener("click",()=>
+    {
+        display.textContent=display.textContent.slice(0,-1);
+    })
+
+clear.addEventListener("click",()=>
+    {
+        display.textContent="";
+    })
+
